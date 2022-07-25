0dda757c2926db1f30cce4ce9e3009c32f549f23
mikeasta <thenaitrise@gmail.com>
2022-07-25 14:23:25 +0300

    Flask server initialized.

--- build.py
+++ build.py
@@ -1,8 +1,15 @@
 # 1. Compile Protobuf's
 import os
-from config import PATH_TO_PROTOBUF_SRC, PATH_TO_PROTOBUF_DST, PROTOBUF_FILENAME
+from config import \
+    PATH_TO_PROTOBUF_SRC,\
+    PATH_TO_PROTOBUF_DST, \
+    PROTOBUF_FILENAME
 
 command = f'protoc -I={PATH_TO_PROTOBUF_SRC} --python_out={PATH_TO_PROTOBUF_DST} {PATH_TO_PROTOBUF_SRC}/{PROTOBUF_FILENAME}.proto'
 os.system(command)
 
 # 2. Run server 
+SERVER_FILENAME = 'server'
+os.system('. venv/bin/activate')
+os.system(f'export FLASK_APP={SERVER_FILENAME}')
+os.system('flask run')
