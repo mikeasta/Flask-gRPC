737651df48891c8ea9026bfdaa3c503c405e966e
mikeasta <thenaitrise@gmail.com>
2022-08-01 14:11:58 +0300

    Image loading is availible, but there is no drawer

--- grpc/client.py
+++ grpc/client.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
-
+from dotenv import load_dotenv
+from PIL import Image
 import settings
 from pb_handler import API_Client
 
+load_dotenv()
 app = Flask("client")
 
 app.config["api"] = API_Client(f"{settings.BACKEND_HOST}:{settings.BACKEND_PORT}")
@@ -10,4 +13,8 @@ app.config["api"] = API_Client(f"{settings.BACKEND_HOST}:{settings.BACKEND_PORT}
 @app.route("/")
 def home():
     api = app.config['api']
-    return str(api.NDArrayRequest([[[1], [2], [3]],[[4], [5], [6]], [[7], [8], [9]]]))
+    res = api.ImageRequest()
+    [width, height, image_data] = res
+    image = Image.frombytes("RGBA", (width, height), image_data)
+    image.save(os.getenv("OUTPUT_IMAGE_NAME"))
+    return f"Width: {width}, height: {height}"
--- grpc/pb_handler.py
+++ grpc/pb_handler.py
@@ -1,6 +1,9 @@
-from urllib import response
+import os
+import io
 import grpc 
 import numpy as np
+from dotenv import load_dotenv
+from PIL import Image
 from numproto import ndarray_to_proto, proto_to_ndarray
 from msg_pb2 import StringMessage, NDArrayMessage, ImageMessage, Response
 from msg_pb2_grpc import FlaskServiceServicer, FlaskServiceStub
@@ -12,6 +15,9 @@ class API_Server(FlaskServiceServicer):
 
     def NDArrayRequest(self, request, context):
         return NDArrayMessage(ndarray=request.ndarray)
+    
+    def ImageRequest(self, request, context):
+        return ImageMessage(width=request.width, height=request.height, image_data=request.image_data)
 
 # Request client handler
 class API_Client:
@@ -29,4 +35,16 @@ class API_Client:
         protobuf = NDArrayMessage(ndarray=ndarray)
         response = self.client.NDArrayRequest(protobuf)
         return proto_to_ndarray(response.ndarray)
+
+    def ImageRequest(self):
+        load_dotenv()
+        INPUT_IMAGE_NAME = os.getenv("INPUT_IMAGE_NAME")
+        image = Image.open(INPUT_IMAGE_NAME)
+        width, height = image.size[0], image.size[1]
+        imageByteArr = io.BytesIO()
+        image.save(imageByteArr, format=image.format)
+        image_data = imageByteArr.getvalue()
+        protobuf = ImageMessage(width=width, height=height, image_data=image_data)
+        response = self.client.ImageRequest(protobuf)
+        return [response.width, response.height, response.image_data]
 
